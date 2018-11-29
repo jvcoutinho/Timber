5091e75a7d65448746bac05b320e1a72561e9293
João Victor <joao.coutinho9@gmail.com>
2018-11-28 21:55:10 -0300

    Setando background

---
+++ script/main.js
@@ -0,0 +1,6 @@
+// Definindo o canvas.
+var canvas = document.querySelector("#canvas");
+canvas.setAttribute("width", window.innerWidth);
+canvas.setAttribute("height", window.innerHeight);
+
+var context = canvas.getContext("2d");
