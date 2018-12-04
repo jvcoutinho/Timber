ccad2a678940571abee20bb24d7fa25f39849358
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2018-12-04 14:05:55 -0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,5 +4,15 @@ canvas.setAttribute("width", window.innerWidth);
 canvas.setAttribute("height", window.innerHeight);
 
 var context = canvas.getContext("2d");
+
+console.log("Na main antes do start");
 start();
+console.log("Na main apos o start\n");
+console.log("Na main antes o chamar updateBranches");
+console.log("Imprimindo o vetor de galhos antes de atualizar os galhos");
+console.log(branchPositions);
+updateBranches();
+console.log("Imprimindo o vetor de galhos após de atualizar os galhos");
+console.log(branchPositions);
+console.log("Na main apos o chamar updateBranches");
 setTimeout(loop, 60);
