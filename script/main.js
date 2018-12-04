d43f1fce2a6e131f3eb6bcb94211fe3812e5fa4b
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2018-12-04 15:52:44 -0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,7 +4,6 @@ canvas.setAttribute("width", window.innerWidth);
 canvas.setAttribute("height", window.innerHeight);
 
 var context = canvas.getContext("2d");
-
 console.log("Na main antes do start");
 start();
 console.log("Na main apos o start\n");
